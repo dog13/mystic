b1e81955c966886ca2855fd267e76b5a185770ff
dog13 <victor@victors-mbp.lan>
2024-03-26 13:12:32 -0700

    dfd

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7k)l^@x+#w%qa7a!-$fdfn_056)_4fe$fqh+zdv75(&e=(xs!3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
